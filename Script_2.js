5f25569915fa7904c32f2d28cd8e4b5d281ac0a9
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-02-25 12:34:02 +0530

    Initialize variables and log their values
    Add variable declarations, initializations, and console logs for fruits, animal, name, and age.

---
+++ Script_2.js
@@ -0,0 +1,32 @@
+/*console.log("Hello wrold")
+console.log(1234)*/ //-----> double or multipal line comment 
+
+//------------> single line comment 
+
+let fruits   // -->datatype and variable declaration
+fruits = "apple"  // --> data inilization 
+console.log(`The Fruits is ${fruits}`)  // print the variable  with string 
+
+let animal
+animal = "tiger"
+console.log(`The Fruits is ${fruits}`)
+
+let a = "apple"
+let c = `apple`
+
+
+let name = "pranav"
+let age = 20;
+age = 21; 
+age = 22; // change the data in realtime is called dynamic 
+
+
+//-----------------This are type of printing the variable data with string-------------------//
+console.log(`my name is ${name} my age is ${age}`)  
+console.log("my name is" ,`${name}`,"my age is",`${age}`)
+console.log("my naem is",name ,"my age is",age)
+console.log("my naem is"+ name +"my age is "+ age)
+
+age = false
+age = true
+console.log(age)
