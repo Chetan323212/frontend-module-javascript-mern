aea961f17175ba61b1865098190c8b77c939e034
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-02-27 07:31:47 +0000

    operator 2

---
+++ Script_4.js
@@ -0,0 +1,65 @@
+//console.log("Math")
+// math function 
+
+// let num = 5.451290
+// console.log(num)
+// console.log(num.toFixed(0))
+// console.log(num.toFixed(1))
+// console.log(num.toFixed(2))
+// console.log(num.toFixed(3))
+// console.log(num.toFixed(4))
+
+
+/************************************************************************* */
+
+
+//parseInt
+// console.log("44")  
+// console.log(parseInt("44"))  // allways return a integer  value
+// console.log(parseInt("44.45"))  // only print a Integer number 
+// console.log(parseInt("-44"))  
+// console.log(parseInt("px"))
+// console.log(parseInt("44px44px44")) 
+// console.log(parseInt("444iii444444iii444"))
+
+
+/******************************(ParseFloat)*********************************************/
+
+let number = "30.12" // 
+number = 30000000000000000000000000000000.00000333
+console.log(parseFloat(number))
+
+/*******************************(Comparison operator)***********************************/
+
+
+// //less then and greater then 
+// console.log(2<3) // TRUE
+// console.log(2>3) // FALSE
+// console.log(2<2) // FALSE
+
+// // less then equal to or greater then equal
+
+// console.log(2<=2) //true
+
+// // equal to = vs == vs ===
+
+// console.log(3 == "3") // true 
+// console.log(3 === "3") // false
+
+// console.log("" == 0) // empty stirng are allways 0 
+// console.log("" == 0)
+
+// // string comparisons 
+// console.log("Glow" == "Glee") // flase
+// console.log("Glow" > "Glee") // true
+// console.log("Bee" > "Be") // true
+
+// compare in different type 
+// when comparing 2 values if any value is number js will convert the other to a number 
+// the string 2 gets converted into a number and then comparison occurs
+//console.log("2" < 1) // false
+
+// //true converted into a number 
+// console.log(true == 0 ) // false 
+// console.log(false == 0 ) // true
+
