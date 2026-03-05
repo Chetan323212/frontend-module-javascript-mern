b0c50409748dccc8f9a63ef988a6a36869e12e78
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-03-05 07:32:01 +0000

    Day_6

---
+++ Script_6.1.js
@@ -0,0 +1,55 @@
+
+// console.log(true || true)  //true
+// console.log(true || false) // true
+// console.log(false || true) // true
+// console.log(false || false) // flase
+
+// let a = 0
+// let b = 0
+// let c = false
+
+// let result = (a || b || c)
+// console.log(result)
+
+// let firstName = ""
+// let LastName = ""
+// let userName = "" 
+
+// let result = (firstName || LastName || userName || null)
+// console.log(result)
+
+
+
+/*---------------------------------------------(And operator)------------------------------------------------------------------------------ */
+
+
+
+// console.log(true && true)  //true
+// console.log(true && false) // false
+// console.log(false && true) // false
+// console.log(false && false) // false
+
+// let age = 20
+// let isfemale = false
+// if(age>18 && isfemale == true){
+//     console.log("its a female adult")
+// }
+// else{
+//     console.log("its is not an adult ot female")
+// }
+
+
+/*----------------------------------------------------(not operator)--------------------------------------------------------------------------------------*/
+
+// let isfemale = true  // true
+// isfemale = !isfemale  // convert into flase 
+// console.log(isfemale) // print
+
+// console.log(!true) // false
+// console.log(!false) // ture
+// console.log(!0) // true
+// console.log(!1) // flse
+
+//boolen conversion 
+console.log(Boolean(0))
+
---
+++ Script_6.js
@@ -0,0 +1,71 @@
+/***********************************Questions***************************** */
+
+// Q1 you are given a number n and you have to print if it is positive ,nigitive or zero
+
+// let num = 2
+// if (num == 0) {
+//     console.log("number is zero")
+
+// } else if (num < 0) {
+//     console.log("number is nigitive")
+// }
+// else {
+//     console.log("number is positive")
+// }
+
+
+// Q2 you are given a number can be in decimals -> check if the integer part is odd or even  
+
+// let n = 223333333.3
+// let  num = Math.floor(n)
+// if(num%2==0){
+//     console.log("Number is even")
+// }
+// else{
+//     console.log("Number is odd")
+// }
+
+
+ //let result = (n>0)? "positive" :(n<0)? "Nigitive" : "Zero"
+// let n = 22.2
+// let result = (Math.floor(n)%2==0)? "even" :"odd"
+
+// let a , b , c 
+// a = -12
+// b = -13
+// c = -14
+
+// if(a>b) {
+//     if(a>c){
+//         console.log("a")
+//     }
+// }
+// if(b>a){
+//     if(b>c){
+//         console.log("b")
+//     }
+// }
+// if(c>a){
+//     if(c>b){
+//         console.log("c")
+//     }
+// }
+
+
+let a = 12
+let b = 13
+let c = 14
+
+if(a>b && a>c ){
+   console.log("a")
+    
+}else if(b>a && b>c){
+    console.log("b")
+}
+else {
+    console.log("c")
+}
+
+
+
+
