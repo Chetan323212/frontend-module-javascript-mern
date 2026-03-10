6357e7de6fa3251f336d38cca56830508dd71207
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-03-10 06:52:43 +0000

    functions

---
+++ Script_9.js
@@ -0,0 +1,62 @@
+//  task is implemnt 4 function for addition, subtraction, multiplication and devision 
+
+// function addition(a ,b){
+
+//     console.log(a+b)
+// }
+// function subtraction(a ,b){
+
+//     console.log(a-b)
+// }
+// function  multiplication(a ,b){
+
+//     console.log(a*b)
+// }
+// function devision(a ,b){
+//     if(b!=0){
+        
+//     console.log(a/b)
+
+//     }
+// }
+
+// addition(4,4)
+// subtraction(4,4)
+// multiplication(4,4)
+// devision(4,4)
+
+// let username = "john"
+// function showmassage(){
+//         username = "bob";
+//         let massage = "hello " + username;
+//         console.log(massage)
+// }
+// console.log(username) // john
+// showmassage(); //hello bob
+// console.log(username) // bob
+
+
+let length1 = 10
+let width1  = 5
+
+function area(l,w = 100){
+    console.log("l " + l)
+    console.log("w " + w)
+    console.log(l*w)
+}
+
+area(length1,width1)
+let fname = "chetan"
+let lname = "patil"
+
+function fullName(){
+    console.log(fname + " " + lname )
+}
+
+fullName("chetan" , lname)
+
+
+
+
+
+
