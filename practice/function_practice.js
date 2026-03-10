c16dfdf1725c787f7a176133459ec731ed93bdfa
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-03-10 11:18:26 +0000

    function and loops practice

---
+++ practice/function_practice.js
@@ -0,0 +1,27 @@
+//  start with the "function" Keyword 
+
+// syntex
+// ----------> 
+// function FunctionName(Parameter){
+
+//        block of code
+
+//}
+// let firstname = "Rohit "
+// let lastname = "Patil"
+
+// function fullName(firstname , lastname){
+//     console.log( firstname + lastname)
+// }
+
+// fullName(firstname , lastname)
+
+// function without parameter 
+
+// function optGeneraction(){
+//     let otp = Math.floor(Math.random()*100000)+2
+//     console.log(`The OTP: ${otp}`)
+// }
+// optGeneraction()
+
+
