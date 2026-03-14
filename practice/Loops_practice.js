fcde8aeeaf368877d323586272588ee57a4bd9c9
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-03-14 11:28:03 +0000

    loops

--- practice/Loops_practice.js
+++ practice/Loops_practice.js
@@ -1,7 +1,7 @@
 //1. Print numbers from 1 to 10
 // 
 // for(let i = 1 ; i<=10; i++){
-    // console.log(i)
+// console.log(i)
 // }
 
 //--------------------------------------------------------------------------------
@@ -9,10 +9,10 @@
 // 2. Print even numbers from 1 to 20
 // 
 // for(let i = 0;i<=20;i++){
-    // if(i%2==0){
-        // console.log(i)
-    // }
-    // 
+// if(i%2==0){
+// console.log(i)
+// }
+// 
 // }
 
 //---------------------------------------------------------------------------------
@@ -20,7 +20,7 @@
 //3.Print numbers in reverse (10 → 1)
 // 
 // for(let i = 10; i>0; i--){
-    // console.log(i)
+// console.log(i)
 // }
 
 //---------------------------------------------------------------------------------
@@ -29,10 +29,121 @@
 // 
 // let sum = 0
 // for(let i = 0; i<=10; i++){
-    // sum += i
+// sum += i
 // }
 // 
 // console.log(sum)
 
 //---------------------------------------------------------------------------------
 // //5. Sum of even numbers
+
+// let sum = 0
+
+// for(let i = 1; i<=10; i++){
+//     if(i%2==0){
+//         sum +=i
+//     }
+// }
+// console.log(sum)
+
+//----------------------------------------------------------------------------------
+// Print multiplication table of 5
+
+// for(let i = 1; i<=10; i++){
+//     let table = i*5
+//     console.log(`5 X ${i} = ${table}`)
+// }
+
+//----------------------------------------------------------------------------------
+
+// //8. Count digits in a number
+// let num = 12345
+// let count = 0
+// let i = 0
+// while (num > 0) {
+//     num = Math.floor(num / 10)
+//     count++
+// }
+// console.log(count)
+
+// // method 2 form gpt 
+// num = 1234562
+// count = num.toString().length
+// console.log(count)
+
+
+// //method 2 
+// let num = 12345;
+// let count = 0;
+
+// if (num === 0) {
+//     count = 1;
+// }
+
+// while (num > 0) {
+//     num = Math.floor(num / 10);
+//     count++;
+// }
+
+// console.log(count);
+
+//----------------------------------------------------(level 2)-------------------------------------------------------------
+
+//1️⃣ Print odd numbers
+
+// for(let i = 1;i<=15; i++){
+//     if(i%2==!0){
+//         console.log(i)
+//     }
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------
+//Reverse a number Input: 12345
+// /num = 1234
+// sum = 0
+// let i = 0
+
+
+// while(num > 0 ){
+//     let digits = num%10
+//     digits.toFixed(1)
+//     num = Math.floor(num/10)
+//     // console.log(digits)
+//     digits.toString()
+//     console.log(digits + " ")
+// }
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+// Sum of digits  Input: 1234
+// num = 1234
+// sum = 0
+// let i = 0
+
+
+// while(num > 0 ){
+//     let digits = num%10
+//     num = Math.floor(num/10)
+//     sum = sum + digits
+
+// }
+
+// console.log(sum)
+
+//----------------------------------------------------------------------------------------------------------------------------
+//4️⃣ Check Prime Number
+
+// let num = 2
+// if(num%2 ==!0){
+   
+//         console.log("prime")
+
+// }
+// else(
+//     console.log("Not prime")
+// )
+
+//----------------------------------------------------------------------------------------------------------------------------
+
