3f730de490874cd9a42c7ee36d956894660504de
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-03-18 07:10:24 +0000

    Array practice

---
+++ practice/Array_practice.js
@@ -0,0 +1,26 @@
+// their is a two type of creating a array so 
+//1) let arr = []
+// 2) array constructer
+
+
+// let arr = [ 1 , 2, 3, 4, 5 ];
+// console.log(arr);
+
+// crate using the constructer
+
+let brr = new Array("chetan", "patil", 22, true );
+//console.log(brr)
+
+// brr.push("last element")
+// brr.push(20 , 21, 22, 23, 34)
+// console.log(brr)
+
+// brr.pop()
+// console.log(brr)
+
+// console.log(brr.slice(5,8))
+// console.log(brr)
+
+// console.log(brr.splice(2, 1 , 21))
+console.log(brr.splice())
+console.log(brr)
