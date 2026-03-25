a15c52b519106f7176b140143a0fd2851eaff9e2
ChetanPatil <95631630+Chetan323212@users.noreply.github.com>
2026-03-25 10:28:45 +0000

    Loops practice1

---
+++ practice/Loops1_practice.js
@@ -0,0 +1,41 @@
+// 1. Print Numbers
+// for(let i = 1; i<=10;i++){
+//     console.log(i)
+// }
+
+
+//---------------------------------------------------------------------------
+
+//2. Even Numbers
+//Print all even numbers from 1 to 50.
+
+// for(let i = 1; i < 50; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+
+// }
+
+//------------------------------------------------------------------------------
+
+//Sum of N Numbers
+// Take a number n and print the sum from 1 to n.
+// 👉 Example: n = 5 → Output = 15
+
+// let sum = 0;
+// let n = 5
+// for(let i = 1; i <= n ; i++){
+//     sum +=i
+// }
+// console.log(sum)
+
+//--------------------------------------------------------------------------------
+
+
+//4. Multiplication Table
+// Print the multiplication table of a number n.
+// 👉 Example: n = 3
+let n = 3
+for(let i = 1 ; i <=10 ; i++){
+    console.log(`${n} X ${n} = ${i*n}`)
+}
